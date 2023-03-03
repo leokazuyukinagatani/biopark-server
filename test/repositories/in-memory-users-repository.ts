9feb591163045b71b5608268f8aae369d15a4c21
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-03-03 09:00:50 -0300

    fix: user in memory test

--- test/repositories/in-memory-users-repository.ts
+++ test/repositories/in-memory-users-repository.ts
@@ -1,35 +1,44 @@
 import { UserRepository } from '../../src/api/repositories/UserRepository'
 
-interface  UserCreateRequest {
+interface UserCreateRequest {
   name: string
   email: string
   password: string
 }
 
-interface  UserShowResponse {
+enum Role {
+  USER = 'USER',
+  ADMIN = 'ADMIN'
+}
+
+interface UserShowResponse {
   name: string
   email: string
   password: string
-  id: string 
+  id: string
   createdAt: Date
-  updatedAt: Date 
-  imageId: string 
+  updatedAt: Date
+  imageId: string
+  role: Role
 }
+
 export class InMemoryUsersRepository implements UserRepository {
-  public users : UserCreateRequest[] = [] 
-  
-  async create({name, email, password}: UserCreateRequest) {
-    const user = {name,email,password}
+  public users: UserCreateRequest[] = []
+
+  async create({ name, email, password }: UserCreateRequest) {
+    const user = { name, email, password }
     this.users.push(user)
   }
 
-  async showByEmail(userEmail: string){
+  async showByEmail(userEmail: string) {
     const user = this.users.find((item) => item.email == userEmail)
 
-    if(!user) {
+    if (!user) {
       return null
     }
     return user as UserShowResponse
   }
- 
+  async update() {
+    
+  }
 }
