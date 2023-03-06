492ba556ddcf9b83630e31698750862fffa6f257
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-03-06 11:44:53 -0300

    feat: zod error catch

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,34 +1,43 @@
-import 'dotenv/config'
-import 'express-async-errors'
-import express from 'express'
-import { Request, Response, NextFunction } from 'express'
-import cors from 'cors'
-import { routes } from './routes'
-import { AppError } from './utils/AppError'
+import "dotenv/config";
+import "express-async-errors";
+import { Request, Response, NextFunction } from "express";
+import * as express from 'express'
+import * as cors from "cors";
+import { routes } from "./routes";
+import { AppError } from "./utils/AppError";
+import { ZodError } from "zod";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
 app.use(
   (
-    error: AppError,
+    error: AppError | ZodError | PrismaClientKnownRequestError ,
     request: Request,
     response: Response,
-    next: NextFunction,
+    next: NextFunction
   ) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
-        status: 'error',
+        status: "error",
         message: error.message,
+      });
+    }else if(error instanceof ZodError){
+      return response.status(500).json({
+        status: 'error',
+        message: error.errors
       })
     }
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    })
-  },
-)
 
-export { app }
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
+
+export { app };
